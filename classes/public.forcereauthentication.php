9b128178a2e6efd25e34c4fcccd6948c35f03652
shrkey <team@shrkey.com>
2013-02-25 18:24:33 +0000

    Add very public class functionality

--- classes/public.forcereauthentication.php
+++ classes/public.forcereauthentication.php
@@ -6,10 +6,27 @@ if( !class_exists( 'forcereauthenticationpublic') ) {
 
 		function __construct() {
 
+			add_action( 'init', array( &$this, 'check_for_oncer' ), 1 );
+
 		}
 
 		function forcereauthenticationpublic() {
 			$this->__construct();
+		}
+
+		function check_for_oncer() {
+
+			if( is_user_logged_in() ) {
+				// If the user is logged in then grab the current user
+				$user = wp_get_current_user();
+				// See if we have a oncer
+				$reauth = shrkey_get_usermeta_oncer( $user->ID, '_shrkey_force_reauthentication' );
+				if(!empty($reauth)) {
+					// We have a oncer so force remove the logged in cookies
+					@wp_logout();
+				}
+			}
+
 		}
 
 	}
