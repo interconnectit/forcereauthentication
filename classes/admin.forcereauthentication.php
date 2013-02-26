0f61fdaf51ccca322b78bdbd1d6436dd2bfcd3e8
shrkey <team@shrkey.com>
2013-02-26 01:09:37 +0000

    Added code to add in bulk action in users / network users pages

--- classes/admin.forcereauthentication.php
+++ classes/admin.forcereauthentication.php
@@ -33,6 +33,10 @@ if( !class_exists( 'forcereauthenticationadmin') ) {
 
 		function bulk_add_action( $actions ) {
 
+			if ( current_user_can( 'delete_users' ) ) {
+				$actions['bulkuserforcereauthenticate'] = __( 'Log Out', 'forcereauthentication' );
+			}
+
 			return $actions;
 		}
 
@@ -67,6 +71,36 @@ if( !class_exists( 'forcereauthenticationadmin') ) {
 				}
 
 			}
+
+			// This is to attempt to add in some bulk operations
+			add_action( 'all_admin_notices',  array( &$this, 'start_object_to_modify_bulk'), 99 );
+
+		}
+
+		function add_modify_bulk( $content ) {
+
+			$ouroption = "<option value='bulkuserforcereauthenticate'>" . __( 'Log Out', 'forcereauthentication' ) . "</option>\n";
+
+			if( is_multisite() && is_network_admin() ) {
+				//<option value='notspam'>Not Spam</option>
+				$content = preg_replace( "/<option value='notspam'>" . __( 'Not Spam', 'user' ) . "<\/option>/", "<option value='notspam'>" . __( 'Not Spam', 'user' ) . "</option>\n" . $ouroption, $content );
+			} else {
+				if( is_multisite() ) {
+					$content = preg_replace( "/<option value='remove'>" . __( 'Remove' ) . "<\/option>/", "<option value='remove'>" . __( 'Remove' ) . "</option>\n" . $ouroption, $content );
+				} else {
+					$content = preg_replace( "/<option value='remove'>" . __( 'Delete' ) . "<\/option>/", "<option value='remove'>" . __( 'Delete' ) . "</option>\n" . $ouroption, $content );
+				}
+			}
+
+
+			return $content;
+
+		}
+
+		function start_object_to_modify_bulk() {
+
+			// Start the object cache
+			ob_start( array( &$this, 'add_modify_bulk' ) );
 
 		}
 
